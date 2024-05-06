ba29bdd87fb40873b9ff3abbadc106cca22df2d3
jewelrana <tonujewel@gmail.com>
2024-05-07 00:22:16 +0600

    Initial Commit

---
+++ SwiftUiFundamental/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUiFundamental
+//
+//  Created by Jewel Rana on 7/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUiFundamental/SwiftUiFundamentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiFundamentalApp.swift
+//  SwiftUiFundamental
+//
+//  Created by Jewel Rana on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiFundamentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
